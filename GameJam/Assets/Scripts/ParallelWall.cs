ebcdd3c970aec6dc1ee32ac0fc079fd3cae242b6
matenagy <mate.nagy@feat.hu>
2024-03-10 22:46:19 +0100

    fix: parallel wall fix and other fixes | add: more levels, level decorations

--- GameJam/Assets/Scripts/ParallelWall.cs
+++ GameJam/Assets/Scripts/ParallelWall.cs
@@ -4,29 +4,35 @@ using UnityEngine;
 
 public class ParallelWall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 
 	void OnTriggerEnter2D(Collider2D collider)
 	{
-		Debug.Log("Enter");
-		LevelManager.Instance.insideWall = true;
-		gameObject.layer = 0;
+		if (collider.CompareTag("Player"))
+		{
+			Debug.Log("Enter");
+			LevelManager.Instance.insideWall = true;
+			gameObject.layer = 0;
+		}
 	}
 
 	void OnTriggerExit2D(Collider2D collider)
 	{
-		Debug.Log("Exit");
-		LevelManager.Instance.insideWall = false;
-		gameObject.layer = 7;
+		if (collider.CompareTag("Player"))
+		{
+			Debug.Log("Exit");
+			LevelManager.Instance.insideWall = false;
+			gameObject.layer = 7;
+		}
 	}
 }
